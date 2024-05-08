3d90f4ab50d502534c63793a510669d939ee2db9
peterxcli <peterxcli@gmail.com>
2024-05-08 14:02:15 +0000

    finish ex2-1, ex2-2

--- CO_F74114752_HW2/arraymul_improved_version1.c
+++ CO_F74114752_HW2/arraymul_improved_version1.c
@@ -1,3 +1,24 @@
-"improved_loop:\n\t"
-    "vsetvli t0, %[arr_size], e16\n\t"
+"improved_loop:;"
+  "vsetivli t0, 8, e16;"
 
+  "vle16.v v1, (%[x]);"
+  "vle16.v v2, (%[h]);"
+  "vmul.vv v3, v1, v2;"
+  "vadd.vx v3, v3, %[id];"
+  "vse16.v v3, (%[y]);"
+
+  "sub %[arr_size], %[arr_size], t0;"
+
+  "slli t0, t0, 1;"
+  "add %[x], %[x], t0;"
+  "add %[h], %[h], t0;"
+  "add %[y], %[y], t0;"
+
+  "addi %[add_cnt], %[add_cnt], 4;"
+  "addi %[sub_cnt], %[sub_cnt], 1;"
+  "addi %[mul_cnt], %[mul_cnt], 1;"
+  "addi %[lw_cnt], %[lw_cnt], 2;"
+  "addi %[sw_cnt], %[sw_cnt], 1;"
+  "addi %[others_cnt], %[others_cnt], 3;"
+
+  "bnez %[arr_size], improved_loop;"
