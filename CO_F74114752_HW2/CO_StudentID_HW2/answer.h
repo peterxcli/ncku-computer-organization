e5592fefae96b9d5c097b595f0e8ba47cbccd88f
peterxcli <peterxcli@gmail.com>
2024-05-08 09:00:09 +0000

    upload unfinished hw2

---
+++ CO_F74114752_HW2/CO_StudentID_HW2/answer.h
@@ -0,0 +1,14 @@
+#define macro_pi_cycle_count pi_cycle_count = 0;
+#define macro_pi_cpu_time pi_cpu_time = 0;
+#define macro_calc_pi_ratio pi_ratio = 0;
+
+
+#define macro_arraymul_baseline_cycle_count arraymul_baseline_cycle_count = 0;
+#define macro_arraymul_baseline_cpu_time arraymul_baseline_cpu_time = 0;
+#define macro_calc_arraymul_baseline_ratio arraymul_baseline_ratio = 0;
+
+#define macro_improved_version1_cycle_count improved_version1_cycle_count = 0;
+#define macro_improved_version1_cpu_time improved_version1_cpu_time = 0;
+
+#define macro_improved_version2_cycle_count improved_version2_cycle_count = 0;
+#define macro_improved_version2_cpu_time improved_version2_cpu_time = 0;
---
+++ CO_F74114752_HW2/CO_StudentID_HW2/arraymul_improved_version1.c
@@ -0,0 +1,3 @@
+"improved_loop:\n\t"
+    "vsetvli t0, %[arr_size], e16\n\t"
+
